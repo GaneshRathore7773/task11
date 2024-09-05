abead2b265f772dff678dd147bceb8226fc7cdf5
GaneshRathore7773 <kunwarganeshsingh777@gmail.com>
2024-09-05 11:49:15 +0530

    main

---
+++ script.js
@@ -0,0 +1,10 @@
+let num = 18;
+let numb = 5;
+
+if (num % 2 === 0) {
+  console.log(num, "is even");
+} else console.log(num, "is odd");
+
+if (numb % 2 === 0) {
+  console.log(numb, "is even");
+} else console.log(numb, "is odd");
